4e4ce798238e5c82a6ed04ca469d17d66f995d21
StephanePetiot <32313519+StephanePetiot@users.noreply.github.com>
2021-02-04 15:56:23 +0100

    Add files via upload

---
+++ files/WorkExperienceSketch.js
@@ -0,0 +1,47 @@
+let workSketch = `
+    <div class = 'sectionHeader'>
+        <img class = 'sectionIcon' src = './files/pictures/workexperience.png'>
+        <span id = 'workSectionLabel'>EXPERIENCE PROFESSIONNELLE</span>
+    </div>
+    <div class = 'sectionContent'>
+        <div class = 'date' id = 'dateWork1' style = ' grid-column: 1; grid-row: 1; '>
+            Février 2020 -</br>
+            Juin 2020
+        </div>
+        <div class = 'job' style = ' grid-column: 2; grid-row: 1; '>
+            <span class = 'positionName' id = 'positionName1'>Ingénieur Big Data | </span>
+            <span class = 'companyCredentials'>Datacorp Innovation – Sophia Antipolis</span>
+            <span class = 'jobDescription' id = 'jobDescription1'>
+                </br>Contribution au développement et à l’industrialisation d’un outil d’aide à la décision pour les analystes business des airlines.</br>
+                <span style = 'font-weight : bold'>Front-end :</span> Angular 9, Tests avec Karma (Jasmine), Protractor</br>
+                <span style = 'font-weight : bold'>Back-end :</span> C++</br>
+                <span style = 'font-weight : bold'>Big Data :</span> Hadoop/Spark, Scala</br>
+                Environnement Agile (Scrum)
+            </span>
+        </div>
+        <div class = 'date' id = 'dateWork2' style = ' grid-column: 1; grid-row: 2; '>
+            Septembre 2016 -</br>
+            Septembre 2019
+        </div>
+        <div class = 'job' style = ' grid-column: 2; grid-row: 2; '>
+            <span class = 'positionName' id = 'positionName2'>Analyste Soutien Logistique Intégré | </span>
+            <span class = 'companyCredentials'>Thales DMS – Sophia Antipolis</br></span>
+            <span class = 'jobDescription' id = 'jobDescription2'>
+                Développement d’une application web de formation (HTML, CSS, JavaScript avec technologie WebGL)</br>
+                Développement d’une solution de traitement pour la maintenance prédictive (Prototypage sous MatLab, programmation de RNN, SVM, etc.)
+            </span>
+        </div>
+        <div class = 'date' id = 'dateWork3' style = ' grid-column: 1; grid-row: 3; '>
+            Avril 2014 -</br>
+            Juin 2014
+        </div>
+        <div class = 'job' style = ' grid-column: 2; grid-row: 3; '>
+            <span class = 'positionName' id = 'positionName3'>Stagiaire Software | </span>
+            <span class = 'companyCredentials'>STMicroelectronics - Sophia Antipolis</br></span>
+            <span class = 'jobDescription' id = 'jobDescription3'>
+                Développement d'un outil de communication I²C à base de STM32 et avec interface PC</br>
+                <span style = ' font-weight : bold; '>Technologies</span> : C++, Qt, STM32, mbed
+            </span>
+        </div>
+    </div>
+`
