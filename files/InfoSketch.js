903789a505e46b3cf1787305617ab8d3837aa4e2
StephanePetiot <32313519+StephanePetiot@users.noreply.github.com>
2021-02-09 23:07:27 +0100

    Add files via upload

---
+++ files/InfoSketch.js
@@ -0,0 +1,57 @@
+let infoSketch = `
+    <div id = 'headlines' style = 'display : grid;'>
+        <div style = 'grid-row : 1; grid-column : 1;'>
+            <img id = 'mainPic' src = './files/pictures/stef.jpg'>
+        </div>
+        <div style = 'grid-row : 2; grid-colum : 1;'>
+            <div id = 'languageButtons' style = 'display : grid;'>
+                <span class = 'languageButton' id = 'frenchButton' style = 'font-weight : bold; grid-row : 1; grid-colum : 1;'>
+                    <img class = 'languageFlags' src = './files/pictures/france.png'>
+                    <span class = 'languageLabels' id = 'frenchLabel'>Français</span>
+                </span>
+                <span class = 'languageButton' id = 'englishButton' style = 'grid-row : 1; grid-colum : 2;'>
+                    <img class = 'languageFlags' src = './files/pictures/uk.png'>
+                    <span class = 'languageLabels' id = 'englishLabel'>English</span>
+                </span>
+            </div>
+        </div>
+        <div id = 'mainInformation' style = 'grid-row : 1; grid-column : 2;'>
+            <div id = 'name'>Stéphane Petiot</div>
+            <div id = 'profileTitle'>Ingénieur Software</div>
+        </div>
+        <div style = 'display : grid'>
+            <a href = 'https://www.linkedin.com/in/stephanepetiot/' class = 'links' style = 'grid-column : 1;'>
+                <img class = 'linksIcon' src = './files/pictures/linkedIn_logo.png'>
+                <span class = 'linksLabel' id = 'linkedInLabel'>LinkedIn</span>
+            </a>
+            <a href = 'https://viadeo.journaldunet.com/p/stephane-petiot-6751312' class = 'links' style = 'grid-column : 2;'>
+                <img class = 'linksIcon' src = './files/pictures/viadeo_logo.png'>
+                <span class = 'linksLabel' id = 'viadeoLabel'>Viadeo</span>
+            </a>
+            <a href = 'https://my.indeed.com/p/stphanep-q2khg7a' class = 'links' style = 'grid-column : 3;'>
+                <img class = 'linksIcon' src = './files/pictures/indeed_logo.png'>
+                <span class = 'linksLabel' id = 'indeedLabel'>Indeed</span>
+            </a>
+            <a href = 'https://www.kaggle.com/stphanepetiot' class = 'links' style = 'grid-column : 4;'>
+                <img class = 'linksIcon' src = './files/pictures/kaggle_logo.png'>
+                <span class = 'linksLabel' id = 'kaggleLabel'>Kaggle</span>
+            </a>
+            <a href = 'https://github.com/StephanePetiot' class = 'links' style = 'grid-column : 5;'>
+                <img class = 'linksIcon' src = './files/pictures/github_logo.png'>
+                <span class = 'linksLabel' id = 'gitHubLabel'>GitHub</span>
+            </a>
+            <a href = 'https://stackoverflow.com/users/14735969/st%c3%a9phane-petiot' class = 'links' style = 'grid-column : 6;'>
+                <img class = 'linksIcon' src = './files/pictures/stackoverflow_logo.png'>
+                <span class = 'linksLabel' id = 'stackOverflowLabel'>StackOverflow</span>
+            </a>
+        </div>
+        <div id = 'profileDescription'>
+            Mon profil est axé autour du développement logiciel et web, avec une spécialisation en IA
+        </div>
+        <div id = 'contactInformation' style = 'display : grid;'>
+            <span style = 'grid-row : 1;'><img class = 'contactIcons' src = './files/pictures/mail.png'>&nbsp;stephane.petiot@hotmail.fr</span>
+            <span style = 'grid-row : 2;'><img class = 'contactIcons' src = './files/pictures/phone.png'>&nbsp;+33 6 67 22 57 50</span>
+            <span style = 'grid-row : 3;'><img class = 'contactIcons' src = './files/pictures/location.png'>&nbsp;Cagnes-sur-Mer, Alpes-Maritimes, France</span>
+        </div>
+    </div>
+`
---
+++ files/MiscellaneousSketch.js
@@ -0,0 +1,53 @@
+const MISC_ICON_PATH = './files/pictures/misc_white.png';
+
+let miscSketch = 
+    "<div class = 'sectionHeader'>"
+        + "<img class = 'sectionIcon' src = " + MISC_ICON_PATH + ">&nbsp;"
+        + `<span id = 'miscSectionLabel'>DIVERS</span>
+    </div>
+    <div class = 'sectionContent'>
+        <div class = 'misc' style = ' grid-column: 1; grid-row: 1; '>
+            <span class = 'miscTopic' id = 'miscTopic1'><u>Certifications</u></span>
+            <dl class = 'miscDescription'>
+                <dt><span class = 'miscName'>TOEFL ITP</span> : 607/677 (2018)</dt>
+                <dt><span class = 'miscName'>TOEIC Listening & Reading</span> : 900/990 (2016)</dt>
+                <dt><span class = 'miscName' id = 'miscCertificationName3'>Certification Voltaire</span> : 816/1000 (2016)</dt>
+            </dl>
+        </div>
+        <div class = 'misc' style = ' grid-column: 2; grid-row: 1; '>
+            <span class = 'miscTopic' id = 'miscTopic2'><u>Langues</u></span>
+            <dl class = 'miscDescription'>
+                <dt>
+                    <img class = 'languageFlag' src = './files/pictures/france.png'>
+                    <span class = 'miscName' id = 'miscLanguageName1'>&nbsp;Français</span> : <span id = 'nativeLanguageLabel'>Langue Maternelle</span>
+                </dt>
+                <dt>
+                    <img class = 'languageFlag' src = './files/pictures/uk.png'>
+                    <span class = 'miscName' id = 'miscLanguageName2'>&nbsp;Anglais</span> : <span id = 'fluentLabel'>Courant</span> (C1)
+                </dt>
+                <dt>
+                    <img class = 'languageFlag' src = './files/pictures/spain.png'>
+                    <span class = 'miscName' id = 'miscLanguageName3'>&nbsp;Espagnol</span> : <span id = 'academicLabel'>Scolaire</span>
+                </dt>
+            </dl>
+        </div>
+        <div class = 'misc' style = ' grid-column-start: 1; grid-column-end: 3; grid-row: 2; '>
+            <span class = 'miscTopic' id = 'miscTopic3'><u>Centres d'intérêts</u></span>
+            <dl class = 'miscDescription' style = ' display : grid '>
+                <span style = ' grid-column : 1; grid-row : 1; '>
+                    <dt class = 'miscName' id = 'miscInterestsName1'>Sport</dt>
+                        <dd id = 'runningLabel'>Course à pied</dd>
+                        <dd id = 'tennisLabel'>Tennis</dd>
+                </span>
+                <span style = ' grid-column-start : 1; grid-column-end : 3; grid-row : 2; '>
+                    <dt class = 'miscName' id = 'miscInterestsName2'>Voyages</dt>
+                        <dd id = 'travelsLabel' style = ' line-height : 1; '>Canada, Etats-Unis, Italie, Belgique, Pays-Bas, Allemagne, Slovénie, Croatie, Monténégro, Albanie, Grèce, Bulgarie, Roumanie, Serbie, Hongrie, Autriche, Slovaquie, Pologne</dd>
+                </span>
+                <span style = ' grid-column : 2; grid-row : 1; '>
+                <dt class = 'miscName' id = 'miscInterestsName3'>Musique</dt>
+                    <dd id = 'guitarLabel'>Pratique de la guitare depuis 2009</dd>
+                </span>
+            </dl>
+        </div>
+    </div>
+`
