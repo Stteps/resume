5b54374c70d2cf7f07d192ab445a65e771b3643e
Stéphane Petiot <stephane.petiot@hotmail.fr>
2021-02-11 14:36:15 +0100

    update

--- files/EmailFormSketch.js
+++ files/EmailFormSketch.js
@@ -1,14 +1,15 @@
 let emailFormSketch = `
     <div id = 'contactTitle'>Contactez-moi</div>
+    <div class = 'closeCross'>x</div>
     <form method = 'POST' id = 'contactForm'>
         <label class = 'emailFormLabel' for = 'name'>Nom</label>
-        <input class = 'emailFormInput' id = 'name' type = 'text' placeholder = 'Entrez votre nom ici' required />
+        <input class = 'emailFormInput' id = 'name' type = 'name' placeholder = 'Entrez votre nom ici' onclick = 'focus()' required />
         <label class = 'emailFormLabel' for = 'email'>Adresse email</label>
-        <input class = 'emailFormInput' id = 'email' type = 'email' placeholder = 'Entrez votre adresse email ici' required />
+        <input class = 'emailFormInput' id = 'email' type = 'email' placeholder = 'Entrez votre adresse email ici' onclick = 'focus()' required />
         <label class = 'emailFormLabel' for = 'subject'>Objet</label>
-        <input class = 'emailFormInput' id = 'subject' type = 'text' placeholder = 'Sujet du message' />
+        <input class = 'emailFormInput' id = 'subject' type = 'subject' placeholder = 'Sujet du message'  onclick = 'focus()' />
         <label class = 'emailFormLabel' for = 'message'>Message</label>
-        <textarea id = 'message' placeholder = 'Entrez votre message ici'></textarea>
-        <button id = 'submitButton' type = 'submit'>Envoyer</button>
+        <textarea id = 'message' placeholder = 'Entrez votre message ici' onclick = 'focus()'></textarea>
+        <button class = 'submitButton' type = 'submit'>Envoyer</button>
     </form>
 `
